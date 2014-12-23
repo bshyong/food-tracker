c8c1724fb5e20cbd4188c1eb0e17971d0441d0c4
Benjamin Shyong <ben.shyong@gmail.com>
2014-12-22 23:22:12 -0800

    add table view and searchcontroller

--- FoodTracker/ViewController.swift
+++ FoodTracker/ViewController.swift
@@ -8,18 +8,66 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate, UITableViewDataSource, UISearchBarDelegate, UISearchControllerDelegate, UISearchResultsUpdating {
 
+  @IBOutlet weak var tableView: UITableView!
+  var searchController: UISearchController!
+  
+  var suggestedSearchFoods:[String] = []
+  var filteredSuggestedSearchFoods:[String] = []
+  
+  
   override func viewDidLoad() {
     super.viewDidLoad()
     // Do any additional setup after loading the view, typically from a nib.
+    self.searchController = UISearchController(searchResultsController: nil)
+    self.searchController.searchResultsUpdater = self
+    self.searchController.dimsBackgroundDuringPresentation = false
+    self.searchController.hidesNavigationBarDuringPresentation = false
+    self.searchController.searchBar.frame = CGRectMake(self.searchController.searchBar.frame.origin.x, self.searchController.searchBar.frame.origin.y, self.searchController.searchBar.frame.size.width, 44.0)
+    
+    // update the tableview header with the search bar
+    self.tableView.tableHeaderView = self.searchController.searchBar
+    // gives access to callbacks from searchBar
+    self.searchController.searchBar.delegate = self
+    // ensure the search results controller is presented inside the view controller
+    self.definesPresentationContext = true
+    
+    self.suggestedSearchFoods = ["apple", "bagel", "banana", "beer", "bread", "carrots", "cheddar cheese", "chicen breast", "chili with beans", "chocolate chip cookie", "coffee", "cola", "corn", "egg", "graham cracker", "granola bar", "green beans", "ground beef patty", "hot dog", "ice cream", "jelly doughnut", "ketchup", "milk", "mixed nuts", "mustard", "oatmeal", "orange juice", "peanut butter", "pizza", "pork chop", "potato", "potato chips", "pretzels", "raisins", "ranch salad dressing", "red wine", "rice", "salsa", "shrimp", "spaghetti", "spaghetti sauce", "tuna", "white wine", "yellow cake"]
   }
 
   override func didReceiveMemoryWarning() {
     super.didReceiveMemoryWarning()
     // Dispose of any resources that can be recreated.
   }
+  
+  // MARK: UITableViewDataSource
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCellWithIdentifier("Cell") as UITableViewCell
+    var foodName : String
+    if self.searchController.active {
+      foodName = filteredSuggestedSearchFoods[indexPath.row]
+    }
+    else {
+      foodName = suggestedSearchFoods[indexPath.row]
+    }
+    cell.textLabel?.text = foodName
+    cell.accessoryType = UITableViewCellAccessoryType.DisclosureIndicator
+    return cell
+  }
 
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if self.searchController.active {
+      return self.filteredSuggestedSearchFoods.count
+    }
+    else {
+      return self.suggestedSearchFoods.count
+    }
+  }
+
+  // MARK: UISearchResultsUpdating
+  func updateSearchResultsForSearchController(searchController: UISearchController) {
+  }
 
 }
 
